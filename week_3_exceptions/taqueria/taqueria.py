b69195a6db229e5964209bf6baa395b33c35e55f
alijanoub-dev <ali.janoub.dev@gmail.com>
2026-09-01 06:34:58 -0700

    week3: solve taqueria assignment

---
+++ week_3_exceptions/taqueria/taqueria.py
@@ -0,0 +1,28 @@
+def main():
+    menue = {
+        "Baja Taco": 4.25,
+        "Burrito": 7.50,
+        "Bowl": 8.50,
+        "Nachos": 11.00,
+        "Quesadilla": 8.50,
+        "Super Burrito": 8.50,
+        "Super Quesadilla": 9.50,
+        "Taco": 3.00,
+        "Tortilla Salad": 8.00
+    }
+
+    total = 0.0
+
+    while True:
+        try:
+            item = input("Item: ").strip()
+
+            if item in menue:
+                total += (menue[item])
+                print(f"Total: ${total:.2f}")
+
+        except EOFError:
+            print()
+            break
+
+main()
